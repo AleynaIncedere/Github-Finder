7c089b4a913821c8cd09ce5aee6095405bfd72b7
AleynaIncedere <103048919+AleynaIncedere@users.noreply.github.com>
2022-04-26 14:43:02 +0300

    Add files via upload

---
+++ github-finder/src/pages/About.jsx
@@ -0,0 +1,26 @@
+function About() {
+  return(
+    <>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+
+        <strong>
+          <a href='https://github.com/AleynaIncedere'>A.Aleyna İNCEDERE</a>
+        </strong>
+        
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By:
+        <a className='text-white' href='https://twitter.com/Redlack1'>
+         A.Aleyna İNCEDERE
+        </a>
+      </p>
+    </>
+    
+  )
+}
+
+export default About
